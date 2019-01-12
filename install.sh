b4c89c068b6f1b1d361d0c6128014c2a30de357a
engd1060 <diogo.mrb@gmail.com>
2019-01-11 22:46:36 -0300

    Autocommit Git-Cron: add 'i3/.config/i3/config'

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function dotfiles(){
+    sudo rm -rf $HOME/.dotfiles
+    sudo cp -r $HOME/github/dotfiles $HOME/.dotfiles
+    cd $HOME/.dotfiles
+}
+
+function stows(){
+stow home
+stow i3
+}
+
+function install(){
+dotfiles
+stows
+}
+
+# Coloque seu usuário
+[[ $USER == "d1060" ]] && install || echo No
+
+# Reiniciar i3
+i3-msg restart
