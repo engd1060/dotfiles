0eb987373df6979bb1fa1d87eff2c3e6d250173c
engd1060 <diogo.mrb@gmail.com>
2019-02-14 23:21:01 -0300

    Autocommit Git-Cron: add 'home/.local/bin/wallpaper.sh'

--- home/.local/bin/wallpaper.sh
+++ home/.local/bin/wallpaper.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # github.com/mamutal91
 
-dir=$HOME/.local/share/wallpapers
-img=wallpaper.jpg
-
-wallpaper=$dir/$img
-
-wal -i $wallpaper -o wal-set
+wal -i $HOME/.local/share/wallpapers -o wal-set
