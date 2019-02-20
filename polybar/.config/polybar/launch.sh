64461de583d4a6d3671d9874f2c9eca54382d591
engd1060 <diogo.mrb@gmail.com>
2019-02-19 21:07:01 -0300

    Autocommit Git-Cron: add 'polybar/.config/polybar/launch.sh'

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-# github.com/mamutal91
 
-wal -c -i $HOME/.local/share/wallpapers/wallpaper.jpg
 
 killall -q polybar
 
-#if type "xrandr"; then
-#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#    MONITOR=$m polybar --reload cima &
-#    MONITOR=$m polybar --reload baixo &
-#  done
-#else
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload cima &
+    MONITOR=$m polybar --reload baixo &
+  done
+else
   polybar --reload cima &
-  #sleep 1s
+  sleep 1s
   polybar --reload baixo &
-#fi
+fi
