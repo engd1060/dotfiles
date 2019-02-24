742bb64bd0e8e4ca95b8b7b12ae0927a681c7be3
engd1060 <diogo.mrb@gmail.com>
2019-02-24 18:57:01 -0300

    Autocommit Git-Cron: add 'polybar/.config/polybar/launch.sh'

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,13 +3,22 @@
 
 killall -q polybar
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload cima &
-    MONITOR=$m polybar --reload baixo &
-  done
-else
-  polybar --reload cima &
-  sleep 1s
-  polybar --reload baixo &
-fi
+while pgrep -u $UID -x ploybar >/dev/null; do sleep 1; done
+
+#--------------------------------------------------------------------
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload cima &
+#    MONITOR=$m polybar --reload baixo &
+#  done
+#else
+#  polybar --reload cima &
+#  sleep 1s
+#  polybar --reload baixo &
+#fi
+#--------------------------------------------------------------------
+
+polybar bar1 &
+polybar bar2 &
+
+echo "Bars launched..."
